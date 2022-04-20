aeb1d188229de2666a60e72b0738be0b581b7c6c
Matt Pocock <mattpocockvoice@gmail.com>
2022-04-20 10:44:42 +0100

    #7 - Hierarchical states and state.matches

--- machines/todoAppMachine.ts
+++ machines/todoAppMachine.ts
@@ -1,12 +1,13 @@
 import { createMachine, assign } from "xstate";
 
 export const todosMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAIC2BDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALqJQAB1nFV6-SAAeiAJwAmCgBYAbAGYA7A4CMW9579bbABoQAE9EAFpbBwAOCjcnWxcnaOinazdU6IBfLOChHAISTgl6UkYhNjAAJyrUKooDABsVADM67Ap8kSLxWlLymTkFJVNNXXMjWBM1UnMrBHCXW3s3NwBWVySNrVdE4LCF6wpo6zWta2tPWy03HfcXHNyQUnQ4cy7CsWo+qQqJ41GcwiLhcWkc0S8Di0AShHic+wingcDgoVxOazOZwcy1WOTyMgKok4FUwJUg-ymgKQlhsR3WCTc1lS0J8bhcCIWnhca2O0Lcnn5Tlua2iGzxIA+RN6kjKmGUg0w1VqVXJ1Mm0zM1PmbnsLmiWmZNyZoPOHPCTicFFBTM8ThFDjWyMZ4slPQpGtmWuBjvBkOhURuzjNS08cTWSzSDsS0LWjyyQA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QBUD2FUAIC2BDAxgBYCWAdmAHQAyquEZUmaGsAxBuRWQG6oDWlZljxEylGnQZN0qWAh6p8uAC7FUpANoAGALqJQAB1nFV6-SAAeiALQBGAMwBWClscA2AJxuA7ACYALI6ODoEANCAAnjb2WrYUbm7+ABy23rZu9v4eSW4AvrnhQjgEJJwS9KSMQmxgAE61qLUUBgA2KgBmjdgURSKl4rQVVTJyCkqmmrrmRrAmaqTmVgjWQVoUvl6Zth72OUneSeFRy472FNu+vt6OO0lXHlf5hTLFopzVmOWQrADCtWAqMCYcgAd2mxgmixs2XiWm8Pk8CV8bmRRxs3m8FHciI8fkc-jcWmyTxAvRKYgofwBqkqwLAIMwyhkFAAyoRUCCpJ1athMGQDABXZSsABiXT5pEFykwRFwlUg4NmkKQlhstl8SQo3nsiTctiSmw8QUcaIQ2w853saUcSSSOuy-nyBRApHQcHMZLeA0ktOqirmZhVSxWNpcWmSyRiCQ8HlNdlxLnstn8WnsmX8ITyzs9-R6I0+gwVKpmAYWQbVVy1iX8GpryN8cP8cf1mu8qfS+MJGNTSRJOYp5SkTJYmDqDX+EH9ytAwcc2ooSRraY8WRjBN8prbFCy9g8cNs2wJ3mSfZefQpVJUUlBjOZbI5XPF-KFU-mUOWbmctsccMSBqSWg+BukSICuFDZDsu5GjqWxZs8GCvLml40owN7Dqgr6BjOaqOL4Wo6gS+qGsapq+HEMaLvibYIgejingh57kJhZbYScCaLr4y6riuKJxla-jxEEnjIjEvhzvCTq5EAA */
   createMachine(
     {
       context: {
         todos: [] as string[],
         errorMessage: undefined as string | undefined,
+        createNewTodoFormInput: "",
       },
       tsTypes: {} as import("./todoAppMachine.typegen").Typegen0,
       schema: {
@@ -15,6 +16,14 @@ export const todosMachine =
             data: string[];
           };
         },
+        events: {} as
+          | {
+              type: "Create new";
+            }
+          | {
+              type: "Form input changed";
+              value: string;
+            },
       },
       id: "Todo machine",
       initial: "Loading Todos",
@@ -36,8 +45,26 @@ export const todosMachine =
             ],
           },
         },
-        "Todos Loaded": {},
+        "Todos Loaded": {
+          on: {
+            "Create new": {
+              target: "Creating new todo",
+            },
+          },
+        },
         "Loading todos errored": {},
+        "Creating new todo": {
+          initial: "Showing form input",
+          states: {
+            "Showing form input": {
+              on: {
+                "Form input changed": {
+                  actions: "assignFormInputToContext",
+                },
+              },
+            },
+          },
+        },
       },
     },
     {
@@ -50,6 +77,11 @@ export const todosMachine =
         assignErrorToContext: assign((context, event) => {
           return {
             errorMessage: (event.data as Error).message,
+          };
+        }),
+        assignFormInputToContext: assign((context, event) => {
+          return {
+            createNewTodoFormInput: event.value,
           };
         }),
       },
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ const Home: NextPage = () => {
   const [state, send] = useMachine(todosMachine, {
     services: {
       loadTodos: async () => {
-        throw new Error("Oh noooooo");
         return ["Take bins out", "Do laundry"];
       },
     },
@@ -19,6 +18,29 @@ const Home: NextPage = () => {
     <div>
       <pre>{JSON.stringify(state.value)}</pre>
       <pre>{JSON.stringify(state.context)}</pre>
+      <div>
+        {state.matches("Todos Loaded") && (
+          <button
+            onClick={() => {
+              send({
+                type: "Create new",
+              });
+            }}
+          >
+            Create new
+          </button>
+        )}
+        {state.matches("Creating new todo.Showing form input") && (
+          <input
+            onChange={(e) => {
+              send({
+                type: "Form input changed",
+                value: e.target.value,
+              });
+            }}
+          ></input>
+        )}
+      </div>
     </div>
   );
 };
