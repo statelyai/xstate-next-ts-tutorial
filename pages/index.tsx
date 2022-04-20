3dbc446d1dc3ce8d34771e356dfa52c9a932a1b7
Matt Pocock <mattpocockvoice@gmail.com>
2022-04-20 09:57:40 +0100

    #2 - Adding my first machine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,31 @@
+import { useMachine } from "@xstate/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { myMachine } from "../machines/myFirstMachine";
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>;
+  const [state, send] = useMachine(myMachine);
+
+  return (
+    <div>
+      {JSON.stringify(state.value)}
+      <button
+        onClick={() => {
+          send("MOUSEOVER");
+        }}
+      >
+        Mouse over
+      </button>
+      <button
+        onClick={() => {
+          send("MOUSEOUT");
+        }}
+      >
+        Mouse out
+      </button>
+    </div>
+  );
 };
 
 export default Home;
